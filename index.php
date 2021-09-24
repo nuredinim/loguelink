13c3c350ea85e9f1aca01146fa9d0496be1dea8a
Mustaf Nuredini <NurediniM@chc.edu>
2021-09-24 10:04:36 -0400

    commit 11

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
 -->
+
+require('../vendor/autoload.php');
+
 <html>
     <head>
         <meta charset="UTF-8">
